6a40b64c7bc9eba1a10be1b82cb1d19a4a4e956d
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2020-09-10 16:07:31 -0400

    User_Profile Created

--- app/src/main/java/com/example/reportcard/ProfileActivity.java
+++ app/src/main/java/com/example/reportcard/ProfileActivity.java
@@ -2,13 +2,66 @@ package com.example.reportcard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.reportcard.model.Student;
+
+import java.util.prefs.PreferenceChangeEvent;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    // Widgets
+    private EditText pFirstName, pLastName, pUsername, pEmail, pPhone;
+    private Button pChangePassword;
+
+    // Variables
+    private SharedPreferences spProfile;
+    private Realm realmProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        realmProfile = Realm.getDefaultInstance();
+
+        findAllViews();
+
+        fetchData();
+    }
+
+    private void findAllViews() {
+        pFirstName = findViewById(R.id.etFirstName);
+        pLastName = findViewById(R.id.etLastName);
+        pUsername = findViewById(R.id.etUsername);
+        pEmail = findViewById(R.id.etEmail);
+        pPhone = findViewById(R.id.etPhone);
+        pChangePassword = findViewById(R.id.btnChangePassword);
+    }
+
+    private void fetchData() {
+        Student student = new Student();
+        spProfile = PreferenceManager.getDefaultSharedPreferences(this);
+        String userName = spProfile.getString("Username", "");
+
+        RealmResults<Student> getData = realmProfile.where(Student.class)
+                .equalTo("sUsername", userName)
+                .findAll();
+
+        realmProfile.beginTransaction();
+        while (getData != null){
+            pFirstName.setText(student.getsFirstName());
+            pLastName.setText(student.getsLastName());
+            pUsername.setText(student.getsUsername());
+            pEmail.setText(student.getsEmail());
+            pPhone.setText(student.getsPhone());
+        }
     }
 }
